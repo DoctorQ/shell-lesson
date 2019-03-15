9219a2d62adcb71f70a7c9a3e8e811bb0a22cd27
doctorq <doctorq@sf-express.com>
2019-03-15 10:50:09 +0800

    update

---
+++ file1.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+awk '
+{
+    for (i = 1; i <= NF; i++) {
+        if (FNR == 1) {
+            t[i] = $i;
+        } else {
+            t[i] = t[i] " " $i
+        }
+    }
+}
+END {
+    for (i = 1; t[i] != ""; i++) {
+        print t[i]
+    }
+}
+' file1.txt
